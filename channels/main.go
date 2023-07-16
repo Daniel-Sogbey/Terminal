8a0959dbe166eb3412cd5157e32faf424c04f13c
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-07-16 08:33:31 +0000

    repeating routines

--- channels/main.go
+++ channels/main.go
@@ -19,8 +19,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
 
 	// fmt.Println(<-c)
@@ -38,10 +38,10 @@ func checkLink(link string, c chan string) {
 
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep its up"
+	c <- link
 }
