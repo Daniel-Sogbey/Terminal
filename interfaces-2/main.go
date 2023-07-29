ceb8ba24cf30f7024ee013c239d8c2b625a32e2a
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-07-29 12:21:26 +0000

    update to using pointers 🥷

--- interfaces-2/main.go
+++ interfaces-2/main.go
@@ -28,23 +28,23 @@ func main() {
 	s := Square{length: 10}
 	c := Circle{radius: 5}
 
-	printArea(r)
-	printArea(s)
-	printArea(c)
+	printArea(&r)
+	printArea(&s)
+	printArea(&c)
 }
 
 func printArea(x Shape) {
 	log.Println(x.area())
 }
 
-func (c Circle) area() float64 {
+func (c *Circle) area() float64 {
 	return math.Pi * math.Pow(c.radius, 2)
 }
 
-func (r Rectangle) area() float64 {
+func (r *Rectangle) area() float64 {
 	return r.length * r.width
 }
 
-func (s Square) area() float64 {
+func (s *Square) area() float64 {
 	return math.Pow(s.length, 2)
 }
