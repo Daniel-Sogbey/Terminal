e3f083676b350bbbe9f59273256046a791478871
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-08-05 09:52:40 +0000

    code clean up

---
+++ hello-world/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// main is the main application function
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Printf("Starting application on port %s", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
+}
