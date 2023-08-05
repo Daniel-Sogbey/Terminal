d9aee8ea301b8f11fab4a02f93f48a565474108c
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-08-05 19:23:27 +0000

    added route handlers

--- hello-world/main.go
+++ hello-world/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -9,17 +10,27 @@ const portNumber = ":8080"
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-
+	renderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
 
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
+
+	if err != nil {
+		fmt.Println("error parsing template, ", err)
+		return
+	}
+
+	parsedTemplate.Execute(w, nil)
 }
 
 // main is the main application function
 func main() {
-
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 
