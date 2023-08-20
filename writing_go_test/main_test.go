8fac25df9e2f8fb92edbbaac49047a8dc055cfb8
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-08-20 23:12:04 +0000

    writing tests with testify

--- writing_go_test/main_test.go
+++ writing_go_test/main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type test struct {
@@ -10,6 +12,21 @@ type test struct {
 	divisor  float32
 	expected float32
 	isErr    bool
+}
+
+func TestDivisionTestify(t *testing.T) {
+	x := 6
+	y := 3
+
+	var expected float32 = 2
+
+	actual, err := divide(float32(x), float32(y))
+
+	if err != nil {
+		assert.Error(t, err)
+	}
+
+	assert.Equal(t, expected, actual)
 }
 
 func TestDivision(t *testing.T) {
