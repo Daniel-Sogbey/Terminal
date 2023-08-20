eb509cc7496af82d854c36cb39978cbb458dbb3d
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-08-20 23:12:47 +0000

    modifications in interfaces

--- interfaces-2/main.go
+++ interfaces-2/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"math"
 )
 
@@ -9,42 +9,47 @@ type Shape interface {
 	area() float64
 }
 
-type Rectangle struct {
-	length float64
-	width  float64
-}
-
 type Square struct {
-	length float64
+	l float64
 }
 
-type Circle struct {
-	radius float64
+type Rectangle struct {
+	l float64
+	b float64
 }
 
 func main() {
-	log.Println("Hello, world!")
-	r := Rectangle{width: 10, length: 20}
-	s := Square{length: 10}
-	c := Circle{radius: 5}
+	square := Square{
+		l: 5,
+	}
 
-	printArea(&r)
-	printArea(&s)
-	printArea(&c)
+	rectangle := Rectangle{
+		l: 5,
+		b: 7,
+	}
+	printArea(rectangle)
+	printArea(square)
+
+	// fmt.Println(areaOfRectange(rectangle))
+	// fmt.Println(areaOfSquare(square))
 }
 
-func printArea(x Shape) {
-	log.Println(x.area())
+// func areaOfRectange(r Rectangle) float64 {
+// 	return r.l * r.b
+// }
+
+// func areaOfSquare(s Square) float64 {
+// 	return float64(math.Pow(float64(s.l), 2))
+// }
+
+func printArea(s Shape) {
+	fmt.Println(s.area())
 }
 
-func (c *Circle) area() float64 {
-	return math.Pi * math.Pow(c.radius, 2)
+func (s Square) area() float64 {
+	return math.Pow(float64(s.l), 2)
 }
 
-func (r *Rectangle) area() float64 {
-	return r.length * r.width
-}
-
-func (s *Square) area() float64 {
-	return math.Pow(s.length, 2)
+func (r Rectangle) area() float64 {
+	return r.l * r.b
 }
