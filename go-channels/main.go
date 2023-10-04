7267e406777c37449990934f2bc2f614c9320c39
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-10-04 15:38:36 +0000

    updated

--- go-channels/main.go
+++ go-channels/main.go
@@ -29,14 +29,36 @@ func main() {
 		}
 	}
 
-	factorial := fact(5)
-	fmt.Println("Factorial", factorial)
+	// factorial := fact(5)
+	// fmt.Println("Factorial", factorial)
 }
 
-func fact(n int) int {
-	if n == 0 {
-		return 1
-	}
-	fmt.Println(n)
-	return n * fact(n-1)
-}
+// func fact(n int) int {
+// 	if n == 0 {
+// 		return 1
+// 	}
+// 	fmt.Println(n)
+// 	return n * fact(n-1)
+// }
+
+// func main() {
+// 	fmt.Println("Enter your amount : ")
+// 	reader := bufio.NewReader(os.Stdin)
+
+// 	input, err := reader.ReadString('\n')
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	input = strings.TrimSpace(input)
+
+// 	amount, err := strconv.ParseFloat(input, 64)
+
+// 	tip := fmt.Sprintf("%v", calculateTip(amount))
+// 	io.Copy(os.Stdout, strings.NewReader(tip))
+// }
+
+// func calculateTip(amount float64) float64 {
+// 	return 0.2 * amount
+// }
