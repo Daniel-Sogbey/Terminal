065b8ab434712327b88b73b49ebdb7200f2264c5
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-11-11 20:35:21 +0000

    tables and db models

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -5,6 +5,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+
+	"github.com/Daniel-Sogbey/api/internal/driver"
+	"github.com/joho/godotenv"
 )
 
 type config struct {
@@ -15,24 +19,43 @@ type application struct {
 	config   config
 	infoLog  *log.Logger
 	errorLog *log.Logger
+	db       *driver.DB
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	var cfg config
 
-	cfg.port = 8081
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+
+	cfg.port = port
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	//INIT DB
+	dsn := os.Getenv("DSN")
+
+	db, err := driver.ConnectPostgres(dsn)
+
+	if err != nil {
+		log.Fatal("Cannot connect to database", err)
+	}
+
+	defer db.SQL.Close()
 
 	app := &application{
 		config:   cfg,
 		infoLog:  infoLog,
 		errorLog: errorLog,
+		db:       db,
 	}
 
-	err := app.serve()
+	err = app.serve()
 
 	if err != nil {
 		log.Fatal(err)
